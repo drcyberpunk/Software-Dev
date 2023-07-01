0cb333b5714b574d7bf0e0f73982ec5f09fbddd4
Aineruhanga Paul <baketpaulo@gmail.com>
2023-07-01 19:43:16 +0300

    merging code

--- base/urls.py
+++ base/urls.py
@@ -1,17 +1,7 @@
 from django.urls import path
-<<<<<<< HEAD
-from . import views
-
-urlpatterns = [
-    path('', views.home, name = "home"),
-    path('room/', views.room, name = "room")
-
-]
-=======
 from . import views 
 
 urlpatterns = [
     path('', views.home, name = "home"),
-    path('room/', views.room, name = "home"),
+    path('room/', views.room, name = "room"),
 ]
->>>>>>> 3847e08240dd8a04a43a9db3faf6999333926c55
--- base/views.py
+++ base/views.py
@@ -1,19 +1,10 @@
 from django.shortcuts import render
-<<<<<<< HEAD
-# Create your views here.
-
-#template inheritance
-
-def home(request):
-    return render(request, 'index.html')
-=======
 #from django.http import HttpResponse
 
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
->>>>>>> 3847e08240dd8a04a43a9db3faf6999333926c55
 
 def room(request):
     return render(request, 'room.html')
